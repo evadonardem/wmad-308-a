7c9ab1cf01a9a144e75e1952c5c53a35477481c5
reymartin <reymartinagluya8@gmail.com>
2025-02-08 13:03:53 +0800

    newwww

--- flutter_02_networking_and_data_agluya_rey/lib/main.dart
+++ flutter_02_networking_and_data_agluya_rey/lib/main.dart
@@ -24,4 +24,38 @@ void main() async {
     print('Detalye ng faker user ${userId}:');
     print('User Details: ${responseSingleUser.body}');
 
+
+    //Fetch Todos
+    var singleUserTodosEndpoint = '${apiBaseUrl}/users/${userId}/todos';
+    var responseSingleUserTodos = await http.get(
+      Uri.parse(
+        singleUserTodosEndpoint
+      )
+    );
+
+    print('Detalye ng faker user ${userId} Todos');
+    print(responseSingleUserTodos.body);
+
+    //Fetch Album
+    var singleUserAlbumEndpoint = '${apiBaseUrl}/users/${userId}/albums';
+    var responseSingleUserAlbum = await http.get(
+      Uri.parse(
+        singleUserAlbumEndpoint
+      )
+    );
+
+    print('Detalye ng faker user ${userId} Album');
+    print(responseSingleUserAlbum.body);
+
+    //Fetch post
+    var singleUserPostsEndpoint = '${apiBaseUrl}/users/${userId}/posts';
+    var responseSingleUserPosts = await http.get(
+      Uri.parse(
+        singleUserPostsEndpoint
+      )
+    );
+
+    print('Detalye ng faker user ${userId} Posts');
+    print(responseSingleUserPosts.body);
+
 }
