cd950f04a8596d00412a867c7e971db26c47794f
robertpaciteng <robertjrpacitengp@gmail.com>
2025-02-08 12:41:23 +0800

    yessir

--- flutter_01_name_app_colsido_jeric/android/app/src/main/kotlin/com/example/flutter_01_name_app_colsido_jeric/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.flutter_01_name_app_colsido_jeric
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity()
---
+++ robert/flutter_02_netwoking_and_data_paciteng_robert/android/app/src/main/kotlin/com/example/flutter_02_netwoking_and_data_paciteng_robert/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.flutter_02_netwoking_and_data_paciteng_robert
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
