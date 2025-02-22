424ec35cda04a958cb158372b213ad5cac67797e
codexcancerion <melbertmarafo2022@gmail.com>
2025-02-22 13:37:07 +0800

    feat: add dto

--- flutter_03_lovely_dogs_marafo_melbert/lib/main.dart
+++ flutter_03_lovely_dogs_marafo_melbert/lib/main.dart
@@ -34,8 +34,8 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  late Future<List<String>> futureBreeds;
-  Future<String>? futureSelectedImageUrl;
+  late Future<List<Breed>> futureBreeds;
+  Future<BreedImage>? futureSelectedImageUrl;
   bool hasSelection = false;
   var wordPair = "";
 
@@ -45,11 +45,11 @@ class _MyHomePageState extends State<MyHomePage> {
     futureBreeds = fetchBreeds();
   }
 
-  void handleBreedSelection(String breed) {
+  void handleBreedSelection(Breed breed) {
     setState(() {
       wordPair = WordPair.random().join("");
       hasSelection = true;
-      futureSelectedImageUrl = fetchRandomAnimalFromBreed(breed);
+      futureSelectedImageUrl = fetchRandomAnimalFromBreed(breed.name);
     });
   }
 
@@ -71,7 +71,7 @@ class _MyHomePageState extends State<MyHomePage> {
                 style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
               ),
             ),
-            FutureBuilder<List<String>>(
+            FutureBuilder<List<Breed>>(
                 future: futureBreeds,
                 builder: (context, snapshot) {
                   if (snapshot.connectionState == ConnectionState.waiting) {
@@ -83,7 +83,7 @@ class _MyHomePageState extends State<MyHomePage> {
                     return DropdownMenu(
                       dropdownMenuEntries: breeds
                           .map((breed) => DropdownMenuEntry(
-                              value: breed, label: breed.toUpperCase()))
+                              value: breed, label: breed.name.toUpperCase()))
                           .toList(),
                       onSelected: (value) {
                         if (value != null) handleBreedSelection(value);
@@ -93,7 +93,7 @@ class _MyHomePageState extends State<MyHomePage> {
                   return const Text("No breeds found.");
                 }),
             if (hasSelection && futureSelectedImageUrl != null)
-              FutureBuilder<String>(
+              FutureBuilder<BreedImage>(
                   future: futureSelectedImageUrl,
                   builder: (context, snapshot) {
                     if (snapshot.connectionState == ConnectionState.waiting) {
@@ -104,7 +104,7 @@ class _MyHomePageState extends State<MyHomePage> {
                       return Padding(
                         padding: const EdgeInsets.all(8.0),
                         child: Image.network(
-                          snapshot.data!,
+                          snapshot.data!.imageUrl,
                           height: 300,
                         ),
                       );
@@ -121,26 +121,47 @@ class _MyHomePageState extends State<MyHomePage> {
   }
 }
 
-Future<List<String>> fetchBreeds() async {
+class Breed {
+  final String name;
+
+  Breed({required this.name});
+
+  factory Breed.fromJson(String name) {
+    return Breed(name: name);
+  }
+}
+
+class BreedImage {
+  final String imageUrl;
+
+  BreedImage({required this.imageUrl});
+
+  factory BreedImage.fromJson(Map<String, dynamic> json) {
+    return BreedImage(imageUrl: json['message']);
+  }
+}
+
+Future<List<Breed>> fetchBreeds() async {
   final dogBreedsEndpoint = 'https://dog.ceo/api/breeds/list/all';
   final response = await http.get(Uri.parse(dogBreedsEndpoint));
 
   if (response.statusCode == 200) {
     final data = jsonDecode(response.body);
-    List<String> breeds = List.from(data['message'].keys);
+    List<Breed> breeds =
+        List.from(data['message'].keys.map((name) => Breed.fromJson(name)));
     return breeds;
   } else {
     throw Exception('Failed to fetch breeds');
   }
 }
 
-Future<String> fetchRandomAnimalFromBreed(String breed) async {
+Future<BreedImage> fetchRandomAnimalFromBreed(String breed) async {
   final dogBreedsEndpoint = 'https://dog.ceo/api/breed/$breed/images/random';
   final response = await http.get(Uri.parse(dogBreedsEndpoint));
 
   if (response.statusCode == 200) {
     final data = jsonDecode(response.body);
-    return data['message'];
+    return BreedImage.fromJson(data);
   } else {
     throw Exception('Failed to fetch image from breed');
   }
