2f2c67adbd4a44ddb576d509d5d9ad5f4e9ac0c8
89chill <prchllee@gmail.com>
2025-04-04 13:48:08 -0500

    hfhf

---
+++ flutter_07_adopt_lovely_dogs_part_2_atuyao_prechel/windows/flutter/ephemeral/generated_config.cmake
@@ -0,0 +1,23 @@
+# Generated code do not commit.
+file(TO_CMAKE_PATH "C:\\Users\\user\\Downloads\\flutter" FLUTTER_ROOT)
+file(TO_CMAKE_PATH "C:\\Users\\user\\Desktop\\wmad\\wmad-308-a\\flutter_07_adopt_lovely_dogs_part_2_atuyao_prechel" PROJECT_DIR)
+
+set(FLUTTER_VERSION "1.0.0+1" PARENT_SCOPE)
+set(FLUTTER_VERSION_MAJOR 1 PARENT_SCOPE)
+set(FLUTTER_VERSION_MINOR 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_PATCH 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_BUILD 1 PARENT_SCOPE)
+
+# Environment variables to pass to tool_backend.sh
+list(APPEND FLUTTER_TOOL_ENVIRONMENT
+  "FLUTTER_ROOT=C:\\Users\\user\\Downloads\\flutter"
+  "PROJECT_DIR=C:\\Users\\user\\Desktop\\wmad\\wmad-308-a\\flutter_07_adopt_lovely_dogs_part_2_atuyao_prechel"
+  "FLUTTER_ROOT=C:\\Users\\user\\Downloads\\flutter"
+  "FLUTTER_EPHEMERAL_DIR=C:\\Users\\user\\Desktop\\wmad\\wmad-308-a\\flutter_07_adopt_lovely_dogs_part_2_atuyao_prechel\\windows\\flutter\\ephemeral"
+  "PROJECT_DIR=C:\\Users\\user\\Desktop\\wmad\\wmad-308-a\\flutter_07_adopt_lovely_dogs_part_2_atuyao_prechel"
+  "FLUTTER_TARGET=C:\\Users\\user\\Desktop\\wmad\\wmad-308-a\\flutter_07_adopt_lovely_dogs_part_2_atuyao_prechel\\lib\\main.dart"
+  "DART_OBFUSCATION=false"
+  "TRACK_WIDGET_CREATION=true"
+  "TREE_SHAKE_ICONS=false"
+  "PACKAGE_CONFIG=C:\\Users\\user\\Desktop\\wmad\\wmad-308-a\\flutter_07_adopt_lovely_dogs_part_2_atuyao_prechel\\.dart_tool\\package_config.json"
+)
